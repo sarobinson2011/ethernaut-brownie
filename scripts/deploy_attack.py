f6908de07ae48cbc68805a38c3ab6e9a77f82f3f
sarobinson2001 <sarobinson2011@gmail.com>
2023-08-15 17:44:32 +0100

    scripts added

--- scripts/deploy_attack.py
+++ scripts/deploy_attack.py
@@ -3,10 +3,10 @@ from brownie import accounts, config, interface, Attack_CoinFlip, network
 
 
 def main():
-    account = get_account()
+    ethernaut_instance = "0x08b6712F4C6675DdD39Eb774be5418c138518469"
 
     # Ethernaut Instance
-    contract = "0x08b6712F4C6675DdD39Eb774be5418c138518469"
+    contract = ethernaut_instance
 
     # Player account
     player = accounts.add(config["wallets"]["from_key"])
@@ -14,8 +14,8 @@ def main():
     # To deploy the attacker contract: To interact with the coinflip contract (i.e. Acting as a proxy)
     # Passing in coinflip_contract address for the constructor to initialise the contract.
     attack_coinflip = Attack_CoinFlip.deploy(contract, {"from": player})
-    print(f"Attack address: {attack_coinflip.address}")
 
+    print(f"Deployed - attack address: {attack_coinflip.address}")
     coinflip_interface = interface.Icoinflip(contract)
 
     for i in range(10):
