c3e38df3db99ca5154213c9349d69f971bbfc3a4
sarobinson2001 <sarobinson2011@gmail.com>
2023-08-18 18:33:49 +0100

    update

--- scripts/04-deploy_attack.py
+++ scripts/04-deploy_attack.py
@@ -13,20 +13,3 @@ def main():
     attack = Attack04.deploy(contract, {"from": player})
     # print(f"Deployed - attack address is: {attack.address}")
     print(f'f"msg.sender = {player.address}\nattack address = {attack.address}')
-
-    # # # # # # # # #   coinflip_interface = interface.ICoinFlip(contract)
-
-    # tx = attack.changeOwner({"from": player})
-    # print("success")
-
-    #       ==>       Print attack.owner by referring to an instance of it ??
-
-
-#     coinflip_interface = interface.ICoinFlip(contract)
-
-#     for i in range(REQUIRED_NUMBER_OF_WINS):
-#         attack.flip({"from": player, "gas_limit": 10000000, "allow_revert": True})
-#         print("Consecutive wins: ", coinflip_interface.consecutiveWins())
-
-
-# # what dies dot .deploy actually do? >
