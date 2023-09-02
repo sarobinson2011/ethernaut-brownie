a239add051f6265b82bebcadb6a678b6b56fe23a
sarobinson2011 <sarobinson2011@gmail.com>
2023-09-02 11:17:41 +0100

    update to 05-attack && 05-deploy_attack

--- scripts/05-deploy_attack.py
+++ scripts/05-deploy_attack.py
@@ -1,13 +1,13 @@
 from scripts.helpful_scripts import get_account
 from brownie import Attack05
 
-ETHERNAUT_INSTANCE = "0x2E5f4De64770d3A1B2740Ac71254929717C43c24"
+# new instance of Level 05 - Token
+ETHERNAUT_INSTANCE = "0x5169932Cd522b4d625dfFE5639EE821EdE2C87c6"
 
 
 def main():
 
     player = get_account()
-
     attacker_contract = Attack05.deploy(ETHERNAUT_INSTANCE, {"from": player})
 
     print(f"attacker contract: {attacker_contract}")
