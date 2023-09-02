211f28da1caa21ccefd91aacbe158a4f604eab08
sarobinson2011 <sarobinson2011@gmail.com>
2023-09-02 21:03:50 +0100

    start fresh 05-attack.sol

--- contracts/05-Token/05-attack.sol
+++ contracts/05-Token/05-attack.sol
@@ -3,23 +3,4 @@ pragma solidity ^0.6.0;
 
 import "./05-token.sol";
 
-contract Attack05 {
-    Token tokenContract;
-
-    constructor(address _tokenContract) public {
-        tokenContract = Token(_tokenContract);
-    }
-}
-
-/*
-
-// import 'interfaces/Itoken.sol';
-
-contract Attack05 {
-  address public owner;
-
-  constructor() public {
-    owner = msg.sender;
-  }
-}
-*/
+contract Attack05 {}
