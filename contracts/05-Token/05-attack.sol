15dff9d9b78201273001745d45db30a04ffdd612
sarobinson2011 <sarobinson2011@gmail.com>
2023-09-02 11:14:30 +0100

    update to 05-attack && 05-deploy_attack

--- contracts/05-Token/05-attack.sol
+++ contracts/05-Token/05-attack.sol
@@ -4,18 +4,22 @@ pragma solidity ^0.6.0;
 import "./05-token.sol";
 
 contract Attack05 {
-    // state variable - stores an instance of Token
-    // contract that we will interact with
     Token tokenContract;
 
-    // constructor takes address of deployed instance
-    // of the Telephone contract to attack
     constructor(address _tokenContract) public {
-        // allows Attack04 to interact with deployed Telephone contract
         tokenContract = Token(_tokenContract);
     }
-
-    // continue from here
-    //  ==> follow - Smart Contract Security - Ethernaut Challenge 05 - Token
-    // YouTube channel: "DeFi Developer Academy"
 }
+
+/*
+
+// import 'interfaces/Itoken.sol';
+
+contract Attack05 {
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+}
+*/
