21c84a7c5e54113f23c73b6a1e20170353d9d128
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-04 10:18:15 +0100

    update 05-deploy_attack.py

--- scripts/05-deploy_attack.py
+++ scripts/05-deploy_attack.py
@@ -2,7 +2,7 @@ from scripts.helpful_scripts import get_account
 from brownie import Attack05
 
 # new instance of Level 05 - Token
-ETHERNAUT_INSTANCE = "0x5169932Cd522b4d625dfFE5639EE821EdE2C87c6"
+ETHERNAUT_INSTANCE = "0x292597B71806e65B2c42b773055AAe6ed09bfC57"
 
 
 def main():
