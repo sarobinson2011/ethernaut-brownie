6779c2b39f1aa2e19fed00f3801108a9d6b58e7c
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-05 11:30:47 +0100

    added 06-Delegation

---
+++ scripts/06-deploy_attack.py
@@ -0,0 +1,10 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface
+
+# new instance of Level 06 - Delegation
+ETHERNAUT_INSTANCE = "0x58817cF27F77243817097b1297D79Ba4d850b233"
+
+
+def main():
+
+    player = get_account()
