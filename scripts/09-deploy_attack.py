77331f65da4bb8875c14c37cddf0dd1cf2efda83
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-13 20:35:31 +0100

    update to 09-attack.sol

--- scripts/09-deploy_attack.py
+++ scripts/09-deploy_attack.py
@@ -2,6 +2,7 @@ from scripts.helpful_scripts import get_account
 from brownie import web3, interface, convert
 
 ETHERNAUT_INSTANCE = "0x36A486BAe1988F4aCB1F9ab5e0606A7A9f635C4f"
+PRIZE = 30000000
 
 
 def main():
