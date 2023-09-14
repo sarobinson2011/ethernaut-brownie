0ea6beb1b2eb793af7ce8624bffaee22efcc0a82
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-14 15:05:31 +0100

    update to interface iking.sol

--- interfaces/iking.sol
+++ interfaces/iking.sol
@@ -2,5 +2,7 @@
 pragma solidity 0.8.0;
 
 interface KingInterface {
+    function prize() external view returns (uint);
+
     function _king() external view returns (address payable);
 }
--- scripts/09-deploy_attack.py
+++ scripts/09-deploy_attack.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account
-from brownie import web3, Attack09, convert
+from brownie import web3, Attack09, convert, interface
 
 ETHERNAUT_INSTANCE = "0x36A486BAe1988F4aCB1F9ab5e0606A7A9f635C4f"
 PRIZE = web3.toWei(0.01, "ether")
@@ -9,7 +9,12 @@ def main():
 
     player = get_account()
 
-    # call prize - read the value - set PRIZE = prize
+    # ----- so:   call prize  ->  read the value  ->  set PRIZE = prize
+    #
+    # prize = interface.KingInterface(ETHERNAUT_INSTANCE)
+    # PRIZE = prize
+    # king = interface.KingInterface(ETHERNAUT_INSTANCE)
+    # print(f'{king} is the KING forever!!!')
 
     attack_contract = Attack09.deploy(
         ETHERNAUT_INSTANCE, {"from": player, "value": PRIZE}
