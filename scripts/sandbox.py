ccf6b79ffadc1094b43e3c800c64521452350ce5
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-26 07:04:56 +0100

    update to sandbox.py

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -3,7 +3,7 @@ from brownie import web3, Attack10, interface
 
 ETHERNAUT_INSTANCE = "0x561Ca003B90617eeca74692FEF51BB20EcAaA19d"
 AMOUNT = "0.001 ether"
-GAS_LIMIT = 600000
+GAS_LIMIT = 6000000
 
 
 def main():
@@ -15,5 +15,11 @@ def main():
     # deploy attack contract     <><><><><><><><><>
     attack = Attack10.deploy(ETHERNAUT_INSTANCE, AMOUNT, {"from": player})
 
+    #
+    print(f"Attack10 balance pre-attack = {web3.eth.get_balance(attack.address())}")
+
     # HERE!!!!!
     attack.attack({"from": player, "allow_revert": True, "gas_limit": GAS_LIMIT})
+
+    #
+    print(f"Attack10 balance post-attack = {web3.eth.get_balance(attack.address())}")
