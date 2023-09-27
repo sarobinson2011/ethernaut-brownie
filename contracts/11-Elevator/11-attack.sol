a73a8046c3792bd0ee1392e9a0d6164e05815276
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-27 18:05:30 +0100

    update 11-elevator.sol

--- contracts/11-Elevator/11-attack.sol
+++ contracts/11-Elevator/11-attack.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Attack11 {}
+interface IBuilding {
+    function isLastFloor(uint) external returns (bool);
+}
+
+contract Attack11 {
+    IBuilding building;
+
+    function attack() external {}
+}
