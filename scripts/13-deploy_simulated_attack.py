b8371997ab00e83d3e63367a1990b466fb12bdc6
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-01 18:44:40 +0100

    added 13-deploy_simulated_attack.py

---
+++ scripts/13-deploy_simulated_attack.py
@@ -0,0 +1,7 @@
+from scripts.helpful_scripts import get_account
+from brownie import web3, interface, Attack13
+
+""" Goal is to simulate Ethernaut level-13 'Gatekeeper One'
+    using Ganache(?) as the first network - update config.yaml
+    ...
+ """
