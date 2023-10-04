60d7c0c7ae13559a85f18591701fd46f67b80aed
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-04 13:15:55 +0100

    add level-14 Gatekeeper Two contracts

--- contracts/14-Gatekeeper-2/14-gatekeeper-2.sol
+++ contracts/14-Gatekeeper-2/14-gatekeeper-2.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/*
+    Again... Register as an entrant to pass this level.  
+*/
+
 contract GatekeeperTwo {
     address public entrant;
 
