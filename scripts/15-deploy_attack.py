5639e52133a96b58edfd6938b622151efdab25ce
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-07 08:06:07 +0100

    update brownie-config.yaml, 15-attack.sol, 15-deploy_attack.py

--- scripts/15-deploy_attack.py
+++ scripts/15-deploy_attack.py
@@ -4,10 +4,15 @@ from brownie import web3, interface, Attack15
 
 ETHERNAUT_INSTANCE = "0x23c8BcEB66E6798E35FE89Dc7fa331ED731B9bFe"
 PLAYER = "0xF8f8269488f73fab3935555FCDdD6035699deE25"
+
 GAS_LIMIT = 6000000
+INITIAL_SUPPLY = "1 ether"
 
 
 def main():
 
     player = get_account()
+
     attack = Attack15.deploy(ETHERNAUT_INSTANCE, {"from": player})
+
+    # attack.attack(ETHERNAUT_INSTANCE, PLAYER, INITIAL_SUPPLY, {"from": player})
