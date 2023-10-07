5be49f12ed1711ec974a5cb4afa45ebccd2c2e13
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-07 15:25:32 +0100

    added level-16 Preservation, plus assoiated files

--- contracts/15-Naught Coin/15-attack.sol
+++ contracts/15-Naught Coin/15-attack.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./15-naught-coin.sol";
-
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Attack15 {
     // state variables
