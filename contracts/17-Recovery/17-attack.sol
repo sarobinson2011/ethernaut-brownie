980ff34b15b55281e68392452bf34debe1b60abf
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-10 12:29:31 +0100

    updated 17-attack.sol

--- contracts/17-Recovery/17-attack.sol
+++ contracts/17-Recovery/17-attack.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "./17-recovery.sol";
+
+contract Attack17 {
+    // state variables
+
+    constructor() {
+        // initialise
+    }
+
+    function attack() external payable {
+        // attack operations
+    }
+}
--- scripts/17-deploy_attack.py
+++ scripts/17-deploy_attack.py
@@ -2,7 +2,7 @@ from scripts.helpful_scripts import get_account
 from brownie import web3, interface, Attack16
 
 
-ETHERNAUT_INSTANCE = ""
+ETHERNAUT_INSTANCE = "0x0c5193d5b956f34248CfD55F3E5b9f4dcA77421F"
 GAS_LIMIT = 600000
 
 
