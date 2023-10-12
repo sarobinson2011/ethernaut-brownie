8428429d17e6ee30f9023874a656d4fd0a7cb30f
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-12 13:16:25 +0100

    updated 18-attack.sol, 18-deploy_attack.py

--- contracts/18-Magic Number/18-attack.sol
+++ contracts/18-Magic Number/18-attack.sol
@@ -4,24 +4,13 @@ pragma solidity ^0.8.0;
 import "./18-magicnumber.sol";
 
 // contract Attack18 {
-//     function attack() external pure returns (address) {
-//         address magicNumber;
-//         assembly {
-//             magicNumber := 0x000000000000000000000000000000000000000000000000000000000000002a
-//         }
-//         return magicNumber;
+//     function attack() public pure returns (uint8) {
+//         return 42;
 //     }
 // }
 
 contract Attack18 {
-    // state variables
-    MagicNum public magicNum;
-
-    constructor(address _targetAddress) {
-        magicNum = MagicNum(_targetAddress);
-    }
-
-    function attack(address _solver) external pure returns (address) {
-        return _solver;
+    function attack() public pure returns (uint8) {
+        return 42;
     }
 }
