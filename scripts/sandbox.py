41cba861f31a15bbc19d662ddc235bd12850d24d
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-13 12:17:15 +0100

    update 'ABI notes' sandbox.py

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,5 +1,5 @@
 """ 
-Contract ABI specification
+                    *** Contract ABI specification ***
 
 Application Binary Interface (ABI) - standard interface for contract interaction
 
@@ -11,6 +11,16 @@ Application Binary Interface (ABI) - standard interface for contract interaction
     2. all contracts have the the interface definitions of other contracts they call.
     3. DOES NOT cover contracts whose interface is dynamic or known only at run-time. 
 
+
+    Function Selector
+
+    The first 4 bytes of the call data for a function call specifies the function to
+    be called. In big endian notation, it is the first 4 bytes of the keccak-256 hash 
+    of the signature of the function.  The 'signature' is the function name with the 
+    parenthesised list of parameter types.  Paramter types are split by a single comma,
+    with no spaces. The return type is NOT part of this signature.
+
+    
     
 
 """
