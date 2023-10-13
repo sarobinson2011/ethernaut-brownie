0d1512fd60a4db0b386a93ebf8bf053648547650
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-13 12:44:45 +0100

    update 'ABI notes' sandbox.py

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,5 +1,5 @@
 """ 
-                    *** Contract ABI specification ***
+                        *** Contract ABI specification ***
 
 Application Binary Interface (ABI) - standard interface for contract interaction
 
@@ -20,7 +20,36 @@ Application Binary Interface (ABI) - standard interface for contract interaction
     parenthesised list of parameter types.  Paramter types are split by a single comma,
     with no spaces. The return type is NOT part of this signature.
 
+    Argument Encoding
+
+    The fifth byte onwards contain the encoded arguments.  This encoding is also used 
+    in other places e.g. the return values and also event arguments are encoded in the
+    same way, without the four bytes specifying the function.
+
+    Types
     
+    https://docs.soliditylang.org/en/v0.8.21/abi-spec.html#types
+
     
+    Mapping Solidity to ABI types
+
+    Solidity supports all types listed in the link above (other than tuples).  Also some
+    Solidity types are not supported by the ABI.  The following mappings show Solidity
+    types that are not part of the ABI <--> the ABI types they map to:
+
+        Solidity            |    ABI
+        --------------------|------------------------------
+        address payable     |    address
+        contract            |    address
+        enum                |    uint8
+        struct              |    tuple
+                            |                    
+        user defined types  |    it's underlying value type          
+        ----------------------------------------------------
+    
+        
+    Encoding Specification 
+
+    https://docs.soliditylang.org/en/v0.8.21/abi-spec.html#design-criteria-for-the-encoding
 
 """
