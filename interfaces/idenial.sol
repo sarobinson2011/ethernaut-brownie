57cb9650473e09f9a69e04aa677d18dddcd17097
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-16 17:48:02 +0100

    updated idenial.sol, 20-deploy_attack.py

--- interfaces/idenial.sol
+++ interfaces/idenial.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IDenial {
-    //
+    function partner() external;
+    function owner() external returns(address);
+    function timeLastWithdrawn() external;
+    function setWithdrawPartner(address _partner) external;
+    function withdraw() external;
+    function contractBalance() external view returns (uint);
 }
--- scripts/20-deploy_attack.py
+++ scripts/20-deploy_attack.py
@@ -3,10 +3,15 @@ from brownie import web3, interface, convert
 from eth_utils import keccak
 
 
-ETHERNAUT_INSTANCE = "0xa5827B90Fd50E8ccCDA14385aaB7966d2F66d5D9"
+ETHERNAUT_INSTANCE = "0x870f1B57394e7a7E5C89f4F2B3111f922CF75555"
 GAS_LIMIT = 6000000
 
 
 def main():
 
     player = get_account()
+    print(player)
+
+    target = interface.IDenial(ETHERNAUT_INSTANCE)
+    print(f"target = {target}")
+    print(f"balance = {target.contractBalance()}")
