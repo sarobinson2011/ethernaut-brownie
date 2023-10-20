b54559af8159e5f5655b4c93d9c0187b0a3de4a0
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-20 17:41:00 +0100

    22-attack.sol, 22-deploy_attack.py

--- contracts/22-Dex/22-attack.sol
+++ contracts/22-Dex/22-attack.sol
@@ -4,18 +4,27 @@ pragma solidity ^0.8.0;
 import "./22-dex.sol";
 
 contract Attack22 {
-    Dex target;
+    Dex _target;
 
     constructor(address _targetAddress) {
-        target = Dex(_targetAddress);
+        _target = Dex(_targetAddress);
     }
 
     function attack() public {
         // stuff
     }
 
-    receive() external payable {
-        // stuff
-    }
+    receive() external payable {}
 }
 
+
+// <><><><><><><>       need to fuck with the swap price       <><><><><><><> 
+//
+// 
+// uint swapAmount = getSwapPrice(from, to, amount);
+// 
+// function getSwapPrice(address from, address to, uint amount) public view returns(uint){
+//    return((amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this)));
+// }
+// 
+// -->  fuck around with the token1 and token2 addresses (in the storage slots) ???
