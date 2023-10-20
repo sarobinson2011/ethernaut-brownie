2c69c376568ddc6c976ed3560fb10478473c7db8
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-20 17:09:16 +0100

    update idex.sol

--- interfaces/idex.sol
+++ interfaces/idex.sol
@@ -2,18 +2,23 @@
 pragma solidity ^0.8.0;
 
 interface IDex {
-    function setTokens(address _token1, address _token2) external;//        onlyOwner
-    function addLiquidity(address tokenAddress, uint amount) external;//    onlyOwner
-    function getSwapPrice(address from, address to, uint amount) 
-        external view returns(uint);//                                      swap price     (view)
-
-    function approve(address spender, uint amount) external;//             - approve token
-    function swap(address from, address to, uint amount) external;//       - swap token
+    function setTokens(address _token1, address _token2) external;//      onlyOwner
+    function addLiquidity(address tokenAddress, uint amount) external;//  onlyOwner
     
-    function balanceOf(address token, address account)//                    token balance  (view) 
+    function getSwapPrice(address from, address to, uint amount) 
+        external view returns(uint);//                                    swap price     (view)
+    function approve(address spender, uint amount) external;//            - approve token
+    function swap(address from, address to, uint amount) external;//      - swap token
+    
+    function balanceOf(address token, address account)//                  token balance  (view) 
         external view returns (uint);                                     
-    function token1() view external;//                                      token1 address (view)
-    function token2() view external;//                                      token2 address (view)
+    function token1() view external;//                                    token1 address (view)
+    function token2() view external;//                                    token2 address (view)
 }
 
-
+// swap price is  -->  amount_to_swap * (token_amount_ratio)
+// 
+// approve address and amount, prior to swap
+// 
+// call --> swap(from, to, amount)
+//      --> 
