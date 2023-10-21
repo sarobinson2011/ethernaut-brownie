1c8485b43752685cfc29d41612c74d1b071689d2
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-21 12:42:45 +0100

    sandbox.py

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -7,8 +7,7 @@
             - we can ignore this as the approve method is included
               in the Dex contract (for ease)
         3. Function swap() is where the token transfer happens
-
-        
+       
         4. Swap price calculation
         
             - (amount * 'to' price) / ('from' price)
@@ -24,5 +23,28 @@
             
             - approve the contract to spend your tokens
 
-
     """
+"""
+    uint swapAmount = getSwapPrice(from, to, amount);
+
+    function getSwapPrice(address from, address to, uint amount) public view returns(uint){
+        
+        swapAmount = (amount * IERC20(to).balanceOf(address(this)))/IERC20(from).balanceOf(address(this))
+        
+        return(swapAmount);
+    }
+    
+"""
+
+# we want a swap_amount > 100
+
+
+def main():
+
+    TOKEN1_AMOUNT = 10
+    TOKEN2_AMOUNT = 10
+    AMOUNT = 2
+
+    swap_amount = AMOUNT * TOKEN1_AMOUNT / TOKEN2_AMOUNT
+
+    print(f"\nSwap amount = {swap_amount}\n")
