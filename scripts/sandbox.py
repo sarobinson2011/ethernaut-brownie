7c93c6bb47800bb9c129c35ab9d9588303922fb2
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-23 10:48:28 +0100

    update sandbox.py

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -36,59 +36,60 @@ def main():
     ETH_player = 10
     WBTC_player = 10
     amount_to_swap = 10
+    toggle = True
 
     for i in range(2):
 
         """
-
         you're swapping between:
 
-        a) the player account (which contains 2 different tokens) , and
+        a) the player account (which contains 2 different tokens) ,and
 
         b) the pool (which contains 2 different tokens)
 
-        you're not swapping between ETH_player and WBTC_player tokens, directly.
-
-        Yeah?!?!
-
-        SO... go fix it then        ---->   it's the values you're passing to swap() !!!!!!!!!
-
         """
 
-        if ETH_player > WBTC_player:
-            A = ETH_player
-            B = WBTC_player
-        else:
-            A = WBTC_player
-            B = ETH_player
-
-        # get the swap "price/amount"
-        _swap = swap(A, B, amount_to_swap)  #  <-- here
-
-        # update pool balances
-        ETH_pool_balance -= _swap
-        WBTC_pool_balance += _swap
-        ETH_player -= _swap
-        WBTC_player += _swap
-
-        print_balances(ETH_pool_balance, WBTC_pool_balance, ETH_player, WBTC_player)
-
-        if ETH_player > WBTC_player:
+        if toggle:
+            A = ETH_pool_balance
+            B = WBTC_pool_balance
             amount_to_swap = ETH_player
         else:
-            amount_to_swap = WBTC_player
+            A = WBTC_pool_balance
+            B = ETH_pool_balance
+            amount_to_swap = ETH_player
+
+        # get the swap "price/amount"
+        swap_amount = swap(A, B, amount_to_swap)
+
+        # update pool balances
+        ETH_pool_balance -= swap_amount
+        WBTC_pool_balance += swap_amount
+        ETH_player -= swap_amount
+        WBTC_player += swap_amount
+
+        print_balances(
+            ETH_pool_balance, WBTC_pool_balance, ETH_player, WBTC_player, toggle
+        )
+
+        toggle = not toggle
+
+        # if ETH_player == 0:
+        #     amount_to_swap = WBTC_player
+        # elif WBTC_player == 0:
+        #     amount_to_swap = ETH_player
 
 
-def swap(balance_1, balance_2, amount):  # <-- update pool balances NOT player balances
+def swap(balance_1, balance_2, amount):
     swap_amount = amount * balance_1 / balance_2
     return swap_amount
 
 
-def print_balances(eth_pool, wbtc_pool, eth_player, wbtc_player):
+def print_balances(eth_pool, wbtc_pool, eth_player, wbtc_player, toggle):
+    print(f"\ntoggle = {toggle}")
     print(f"ETH-pool balance: {eth_pool}")
     print(f"WBTC-pool balance: {wbtc_pool}")
     print(f"ETH-player balance: {eth_player}")
-    print(f"WBTC-player balance: {wbtc_player}")
+    print(f"WBTC-player balance: {wbtc_player}\n")
 
 
 # toggle from
