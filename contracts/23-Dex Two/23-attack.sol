99dc87365ec2c8ebb0a743f2d01194daefbf821b
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-27 11:36:35 +0100

    updated idextwo.sol, 23-attack.sol

--- contracts/23-Dex Two/23-attack.sol
+++ contracts/23-Dex Two/23-attack.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "./23-dextwo.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Attack23 {
     DexTwo public target;
     address public token3;
+    uint256 public token3Balance; 
 
     constructor(address _target, uint256 _amount) {
         target = DexTwo(_target);
-        token3 = _amount;
+        token3Balance = _amount;
     }
 
     function attack() public {
-        //
+        target.swap(from, to, amount);
     }
 
     receive() external payable {}
--- interfaces/idextwo.sol
+++ interfaces/idextwo.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IDexTwo {
-    //
+    function swap(address from, address to, uint amount) external;
+    function getSwapAmount(address from, address to, uint amount) external view returns(uint);
+    function approve(address spender, uint amount) external;
+    function balanceOf(address token, address account) external view returns (uint);
 }
 
 
