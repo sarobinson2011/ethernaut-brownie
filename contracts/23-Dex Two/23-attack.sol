9b3d6261ee416a017d8fc1ae520dfac98a4b061d
sarobinson2001 <sarobinson2011@gmail.com>
2023-10-31 13:34:26 +0000

    update 23-attack.sol

--- contracts/23-Dex Two/23-attack.sol
+++ contracts/23-Dex Two/23-attack.sol
@@ -7,33 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/*
-    Attack philoslophy
-
-    1. any token can be swapped in DexTwo
-    2. so we need to use the native DexTwo swap() function
-    3. we can mimic the other functions - getSwapPrice() and approve() from here
-    4. from, to, amount, spender --> these vars required
-*/
 
 contract Attack23 {
     DexTwo public target;
-    address public token1;
-    address public token2;
     address public token3;
     address public owner;
-    uint256 public token3Player;
-    uint256 public token3Pool; 
 
     constructor(address _target, uint256 amountPool, uint256 amountPlayer) {
         owner = msg.sender;
         target = DexTwo(_target);
-        // set amount of token 3 to 200  <-- here 
+        // set amount of token 3 to 200  (new variable ?)  <-- here 
     }
-
-
-    // function getSwapAmount(address from, address to, uint amount) public view returns(uint)
-    // function approve(address spender, uint amount) public
 
 
     function attack(address _from1, address _from2, uint256 _amount) public {
