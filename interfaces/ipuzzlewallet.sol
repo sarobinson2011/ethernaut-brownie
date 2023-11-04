6fbc34598848ac754c9e06340470fc1389a45a30
sarobinson2001 <sarobinson2011@gmail.com>
2023-11-04 13:00:03 +0000

    update IPuzzleWallet.sol

--- interfaces/ipuzzlewallet.sol
+++ interfaces/ipuzzlewallet.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 interface IPuzzleWallet {
-    // 
+    function owner() external view;
+    function maxBalance() external view;
+    function init(uint256 _maxBalance) external;
+    function setMaxBalance(uint256 _maxBalance) external;
+    function addToWhitelist(address addr) external;
+    function deposit() external payable;
+    function execute(address to, uint256 value, bytes calldata data) external payable;
+    function multicall(bytes[] calldata data) external payable;
 }
+
+
+
+
+
+
