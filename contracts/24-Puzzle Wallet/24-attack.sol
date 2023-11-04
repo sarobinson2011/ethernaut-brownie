f995e5396a2d4b9d67d36ef92376d153513ae48e
sarobinson2001 <sarobinson2011@gmail.com>
2023-11-04 12:01:58 +0000

    update 24-attack.sol, 24-deploy_attack.py

--- contracts/24-Puzzle Wallet/24-attack.sol
+++ contracts/24-Puzzle Wallet/24-attack.sol
@@ -2,4 +2,16 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
+import "./24-puzzlewallet.sol";
 
+contract Attack24 {
+    // variables
+
+    constructor() {
+        // init
+    }
+
+    function attack() public {
+        // stuff
+    }
+}
--- scripts/24-deploy_attack.py
+++ scripts/24-deploy_attack.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account
-from brownie import web3, interface, convert
+from brownie import web3, interface, convert, Attack24
 from eth_utils import keccak
 
 ETHERNAUT_INSTANCE = "0xd206fd0618e91Da883D764c6d891d67591881dbb"
@@ -9,3 +9,5 @@ GAS_LIMIT = 12000000
 
 def main():
     player = get_account()
+    target = interface.IPuzzleWallet(ETHERNAUT_INSTANCE)
+    attack = Attack24.deploy({"from": player})
