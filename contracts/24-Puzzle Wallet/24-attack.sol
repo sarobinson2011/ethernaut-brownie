414447b3aee2cc77dffb63c9e72a277e2dc3d742
sarobinson2001 <sarobinson2011@gmail.com>
2023-11-23 18:34:52 +0000

    updated 24-attack.sol

--- contracts/24-Puzzle Wallet/24-attack.sol
+++ contracts/24-Puzzle Wallet/24-attack.sol
@@ -2,14 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "./24-puzzlewallet.sol";
-import "interfaces/ipuzzlewallet.sol";
-
 
 contract Attack24 {
     PuzzleWallet public target;
     PuzzleProxy public proxy;
 
-    constructor(address _targetAddress) {
+    constructor(address payable _targetAddress) {
         target = PuzzleWallet(_targetAddress);        
         proxy = PuzzleProxy(_targetAddress);
     }
